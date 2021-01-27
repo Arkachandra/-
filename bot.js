952287d51d34dfa2586a57fa2496a3eb0a17f5d5
Arkachandra <78072026+Arkachandra@users.noreply.github.com>
2021-01-28 01:29:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,12 +15,12 @@ bot.on('message', message =>{
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
-                message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
+                message.channel.send("Hey, I only understand !bang right now. More will be added when my creator is free. :) ")
             }
         }
         else{
             message.delete();
-            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+            message.channel.send(sender + " this bot only accepts commands which starts with '!' dot.");
         }
     }
 });
