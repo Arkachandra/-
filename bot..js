89d0576e0e3ed793d7669b62d59278b3513ff2b9
Arkachandra <78072026+Arkachandra@users.noreply.github.com>
2021-01-28 01:36:23 +0530

    Add files via upload

---
+++ bot..js
@@ -0,0 +1,39 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = "!" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "803567842511290378" && message.channel.id === "803701358221328444"){
+        if(msg.startsWith('!', 0)){
+            if(msg === prefix + "bang" && message.channel.id === "803701358221328444"){
+                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand !bang right now. More will be added when my creator is free. :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '!' dot.");
+        }
+    }
+});
+
+client.on("ready", massage => {
+    console.log("I am ready to Go");
+
+}};
+
+
+client.on("massage", massage => {
+    if(massage.content === `${prefix}pong`){
+        if(massage.channel.send(`pong - ${client.ws.ping}`)
+    }
+})
+bot.login(process.env.BOT_TOKEN)
